46eff36a1c5733322a5e980daceac402d3463722
MSTK <yeloz@126.com>
2016-11-25 10:46:55 +0800

    This is version 1.0.0.

--- app/src/androidTest/java/com/xkyiliao/dataapplication/ApplicationTest.java
+++ app/src/androidTest/java/com/xkyiliao/dataapplication/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.xkyiliao.myapplication;
+package com.xkyiliao.dataapplication;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/xkyiliao/dataapplication/MainActivity.java
+++ app/src/main/java/com/xkyiliao/dataapplication/MainActivity.java
@@ -1,4 +1,4 @@
-package com.xkyiliao.myapplication;
+package com.xkyiliao.dataapplication;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.xkyiliao.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
